f94ca610450282d2543a21700552e46d91f9154c
Platon Yasev <yasevplaton@gmail.com>
2021-10-22 22:24:53 +0300

    generate grid geojson on the fly

--- src/api/grid/index.js
+++ src/api/grid/index.js
@@ -1,18 +1,37 @@
 import axios from "../../config/axios";
 import { useQuery } from "react-query";
+import GeoJSON from "geojson";
 
 const URL = "https://geoapi.social.ru.com/api";
 
-const getGrid = async (mapExtent) => {
-  const { lat_min, lat_max, lon_min, lon_max } = mapExtent;
-  const data = await axios.get(
-    `${URL}/poly/?lat_min=${lon_min}&lat_max=${lon_max}&lon_min=${lat_min}&lon_max=${lat_max}`
-  );
-  return data;
+const getGrid = async () => {
+  const data = await axios.get(`${URL}/poly_new/`);
+  const parsedData = data.map((item) => {
+    const { polygon } = item;
+    let coords = polygon.replace("SRID=4326;POLYGON ((", "");
+    coords = coords.replace("))", "").split(",");
+    coords = coords.map((latLon) => {
+      const latLonArr = latLon.trim().split(" ");
+      return latLonArr.map((ll) => {
+        return Number(ll);
+      });
+    });
+
+    return {
+      ...item,
+      coords: [coords],
+    };
+  });
+
+  const geojson = GeoJSON.parse(parsedData, {
+    Polygon: "coords",
+  });
+
+  return geojson;
 };
 
 export const useGetGrid = (mapExtent, enabled) => {
-  return useQuery(["grid", mapExtent], () => getGrid(mapExtent), {
+  return useQuery(["grid"], () => getGrid(), {
     enabled,
     keepPreviousData: true,
   });
