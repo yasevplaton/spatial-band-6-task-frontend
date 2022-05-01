5bd6ca86ed4a6713e75e9a73d73f4223413d1cd2
toren332 <tmalinin@endwork.today>
2022-05-02 00:30:23 +0300

    new endpoint

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "https://social.ru.com/api";
+const API_URL = "https://api.spatiality.endwork.today/";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
